18dda871052e11027e5a874c5a912d5c5949c1f2
herminemn <hermine.mn@gmail.com>
2018-12-19 02:54:08 +0400

    Initial commit

---
+++ uploaddocx/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'uploaddocx/upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'uploaddocx/upload.html')
