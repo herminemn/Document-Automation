1a309e29d351d4d273b1a8fa4aea8467f0b4fae7
herminemn <hermine.mn@gmail.com>
2018-12-19 17:01:34 +0400

    Added Featues for Working with Docx Files

--- uploaddocx/views.py
+++ uploaddocx/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from .forms import UploadFileForm
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
 
 
 def simple_upload(request):
@@ -10,8 +12,17 @@ def simple_upload(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'uploaddocx/upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        document = Document(filename)
+        document.save(filename)
+        my_list = []
+        for p in document.paragraphs:
+            for run in p.runs:
+                if run.italic:
+                    my_list.append(run.text)
+
+        return HttpResponse(my_list)
+
+        # return render(request, 'uploaddocx/upload.html', {
+        #     'filename': filename
+        # })
     return render(request, 'uploaddocx/upload.html')
