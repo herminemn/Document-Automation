53f8fc9a736fbcbd5863e4f906c93a1baa2bdfdb
herminemn <hermine.mn@gmail.com>
2018-12-20 21:52:38 +0400

    Modification in views.py

--- uploaddocx/views.py
+++ uploaddocx/views.py
@@ -4,11 +4,28 @@ from .forms import UploadFileForm
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from docx import Document
-from docx.enum.style import WD_STYLE_TYPE
+import re
 
 
-def work_with_docs(request):
-    pass
+def docx_words_replace(doc_obj, regex, replace):
+
+    for p in doc_obj.paragraphs:
+        if regex.search(p.text):
+            inline = p.runs
+
+            for i in range(len(inline)):
+                if regex.search(inline[i].text):
+                    text = regex.sub(replace, inline[i].text)
+                    inline[i].text = text
+
+    for table in doc_obj.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                docx_words_replace(cell, regex, replace)
+
+
+my_regex = re.compile(r"\{(.*?)\}")
+my_replace = r"replaced_word"
 
 
 def simple_upload(request):
@@ -17,12 +34,16 @@ def simple_upload(request):
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         document = Document(filename)
-        document.save(filename)
-        # [[run.text for run in p.runs if run.italic] for p in document.paragraphs]
+        # docx_words_replace(document, my_regex, my_replace)
         variables = []
-        for p in document.paragraphs:
-            for run in p.runs:
-                if run.italic:
-                    variables.append(run.text)
+        for paragraph in document.paragraphs:
+            match = re.findall(r"\{(.*?)\}", paragraph.text)
+            variables.append(match)
+        for table in document.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    match = re.findall(r"\{(.*?)\}", cell.text)
+                    variables.append(match)
+        document.save('result.pdf')
         return render(request, 'uploaddocx/upload.html', {'variables': variables})
     return render(request, 'uploaddocx/upload.html')
